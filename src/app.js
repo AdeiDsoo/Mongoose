22c29b63e599729c2fd7e8d8c18c0cb038096675
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei.local>
2023-10-22 10:26:37 -0600

    structure for project

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import express from "express";
+import { __dirname } from "./utils.js";
+import handlebars from "express-handlebars";
+import "./db/config.js";
---
+++ src/db/config.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const URI =
+    "mongodb+srv://dsoo:Jaysriradhe@cluster0.edmnjjr.mongodb.net/ecommerce47310?retryWrites=true&w=majority";
+
+mongoose
+    .connect(URI)
+    .then(() => console.log("conectado a la base de datos"))
+    .catch((error) => console.log(error));
