8343b642f8e736b29954da7f2ef0b14ed6add546
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2024-01-20 18:52:24 -0600

    testing

--- test/carts.test.js
+++ test/carts.test.js
@@ -7,15 +7,18 @@ describe("Carts Routes", function () {
 	describe("GET /api/carts/:idCart", () => {
 		it("should retrieve cart by ID", async () => {
 			const idCart = "652b1ee59a8392d89099b7dc";
-
+		
 			const response = await request.get(`/api/carts/${idCart}`);
 
-			
 			expect(response.status).to.equal(200);
 			expect(response.body).to.be.an("object");
 			expect(response.body.cart).to.have.property("productsCart");
-			
 		});
+		it("should fail", async () => {
+            	const failIdCart = "652b1569a8392d89099b7d9";
+			const response = await request.get(`/api/carts/${failIdCart}`);
 
+			expect(response.status).to.equal(500);
+		});
 	});
 });
